8279885e884881bd8bcef13278cbabc6a4deda45
Teaflex <teaflex.dev@hotmail.com>
2024-05-08 10:03:04 +0200

    chore(deps): added svr vite plugin in dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
-  plugins: [react()],
+export default defineConfig(() => ({
+  plugins: [react(), svgr()],
   server: {
     port: 3000,
   },
